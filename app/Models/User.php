92f5504e649c3493d944e1b55f4e0af6b0aa7e64
phamvant <pham.t.286.01@gmail.com>
2022-08-17 09:32:17 +0700

    Chapter4

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,18 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    protected function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['first_name'] . ' ' . $attributes['last_name']
+        );
+    }
+    
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => Str::slug($value, '-'),
+        );
+    }
 }
