59473c72e17a9e9a3ef9f9edb65215d541da9fc6
Hung <chumeolv@gmail.com>
2022-11-01 21:54:07 +0700

    tạo thêm các lựa chọn cho thanh sidebar, t có tạo sắn các form tương ứng với cách lựa chọn trong file resources/pages, có gắn sẵn đường link rồi, mn update db đổ sản phẩm vào đó nhé

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -80,39 +80,57 @@
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('home') }}">
-                        <i class="ni ni-tv-2 text-primary"></i> {{ __('Dashboard') }}
+                        <i class="ni ni-tv-2 text-primary"></i> {{ __('Home') }}
                     </a>
                 </li>
+                
                 <li class="nav-item">
-                    <a class="nav-link active" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-examples">
-                        <i class="fab fa-laravel" style="color: #f4645f;"></i>
-                        <span class="nav-link-text" style="color: #f4645f;">{{ __('Laravel Examples') }}</span>
+                    <a class="nav-link active" href="{{ route('mp') }}">
+                        <i class="fab fa-laravel" style="color: #f4645f;"></i> {{ __('MOUSE PAD') }}
+                        
                     </a>
 
-                    <div class="collapse show" id="navbar-examples">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('users.show', ['user' => auth()->user()->id]) }}">
-                                    {{ __('User profile') }}
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('users.index') }}">
-                                    {{ __('User Management') }}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-planet text-blue"></i> {{ __('Icons') }}
+                    <a class="nav-link" href="{{ route('gg') }}">
+                        <i class="ni ni-planet text-blue"></i> {{ __('GAMING GEAR') }}
+                    </a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ route('da') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('DECOR ACCESSORIES') }}
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link" href="#">
-                        <i class="ni ni-pin-3 text-orange"></i> {{ __('Maps') }}
+                    <a class="nav-link" href="{{ route('smw') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('SPEAKER, MICRO, WEBCAM') }}
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ route('gc') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('GAMING CHAIR ') }}
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ route('gt') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('GAMING TABLE ') }}
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ route('ssu') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('SIGNAL SPLITTER, USB ') }}
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ route('rc') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('RADIATORS, COOLING ') }}
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="{{ route('pa') }}">
+                        <i class="ni ni-pin-3 text-orange"></i> {{ __('PHONE ACCESSORIES ') }}
                     </a>
                 </li>
                 <li class="nav-item">
@@ -121,11 +139,7 @@
                       <span class="nav-link-text">Tables</span>
                     </a>
                 </li>
-                <li class="nav-item mb-5 mr-4 ml-4 pl-1 bg-danger" style="position: absolute; bottom: 0;">
-                    <a class="nav-link text-white" href="https://www.creative-tim.com/product/argon-dashboard-pro-laravel" target="_blank">
-                        <i class="ni ni-cloud-download-95"></i> Upgrade to PRO
-                    </a>
-                </li>
+                
             </ul>
             <!-- Divider -->
             <hr class="my-3">
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,15 @@ Route::get('/dashboard', function () {
 
 Route::get('lang/{lang}', [LangController::class, 'changeLang'])->name('lang');
 
+Route::get('mousepad', [PageController::class, 'mp'])->name('mp');
+Route::get('gaminggear', [PageController::class, 'gg'])->name('gg');
+Route::get('decoraccessories', [PageController::class, 'da'])->name('da');
+Route::get('speakermicrowebcam', [PageController::class, 'smw'])->name('smw');
+Route::get('gamingchair', [PageController::class, 'gc'])->name('gc');
+Route::get('gamingtable', [PageController::class, 'gt'])->name('gt');
+Route::get('signalsplitterusb', [PageController::class, 'ssu'])->name('ssu');
+Route::get('radiatorscooling', [PageController::class, 'rc'])->name('rc');
+Route::get('phoneaccessories', [PageController::class, 'pa'])->name('pa');
 Route::resource('users', UserController::class);
 Route::resource('tasks', TaskController::class);
 
