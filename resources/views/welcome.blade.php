bce7a689a8995bdffc2b9bea9e915da7b1e4f62d
dapham17 <dapham.auth17@gmail.com>
2022-11-04 02:39:36 +0700

    update filter product

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app', ['class' => 'bg-default'])
 
 @section('content')
+    <div>
+        <h1 class="text-white more_txt_wel" style="text-align:center; margin: 50px 0;" >{{ __('Welcome to CozyFarm.') }}</h1>
+    </div>
     <div class="header py-7 py-lg-8 header_wel">
         <div class="container">
             <div class="header-body text-center mt-7 mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white more_txt_wel">{{ __('Welcome to CozyFarm.') }}</h1>
+                        <!-- <h1 class="text-white more_txt_wel">{{ __('Welcome to CozyFarm.') }}</h1> -->
                     </div>
                 </div>
             </div>
